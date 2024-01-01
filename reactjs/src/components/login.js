203a2541660ce2347859a5c5a92b0871ddd121bc
abdulhakim14 <abdulhakimiubat@gmail.com>
2024-01-01 12:25:08 +0600

    noting changed

--- reactjs/src/components/login.js
+++ reactjs/src/components/login.js
@@ -5,6 +5,9 @@ import { Link } from "react-router-dom";
 export default function Login() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const submitForm = () => {
+    console.log(email + "" + password);
+  };
   return (
     <div>
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
@@ -33,6 +36,7 @@ export default function Login() {
                   id="email"
                   name="email"
                   type="email"
+                  onChange={(e) => setEmail(e.target.value)}
                   autocomplete="email"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -62,6 +66,7 @@ export default function Login() {
                   id="password"
                   name="password"
                   type="password"
+                  onChange={(e) => setPassword(e.target.value)}
                   autocomplete="current-password"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -72,6 +77,7 @@ export default function Login() {
             <div>
               <button
                 type="submit"
+                onClick={submitForm}
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
